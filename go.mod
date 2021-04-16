ba295e3e1ec6adaaf1c3d866cfc16fb47a7f4fa8
Mshivam2409 <shivammalhotra08@gmail.com>
2021-04-16 06:47:27 +0530

    Add Cobra Cmd

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require (
 	github.com/dgraph-io/badger/v3 v3.2011.1
 	github.com/gofiber/fiber/v2 v2.7.1
 	github.com/mikespook/gorbac v2.1.0+incompatible
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/ory/kratos-client-go v0.5.5-alpha.4
+	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	github.com/vektah/gqlparser v1.3.1
 	go.mongodb.org/mongo-driver v1.5.1
--- main.go
+++ main.go
@@ -3,25 +3,14 @@ package main
 import (
 	"log"
 
-	"github.com/AnC-IITK/Xenon/internal"
-	"github.com/gofiber/fiber/v2"
-	"github.com/spf13/viper"
+	"github.com/AnC-IITK/Xenon/cmd"
 )
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-	viper.ReadInConfig()
-	internal.PermissionsInit()
-	internal.ConnectMongo()
-	internal.OryClient.Connect()
-	internal.Init()
+
 }
 
 func main() {
-	app := fiber.New()
-	internal.SetupRoutes(app)
-	app.Listen(":5000")
+	cmd.Execute()
 }
