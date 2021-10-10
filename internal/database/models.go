4d74da85c3e1d4a472b0fdbdd64cc9db655897b7
Mshivam2409 <shivammalhotra08@gmail.com>
2021-10-11 03:28:45 +0530

    Update Model to Allow JSON

--- internal/database/models.go
+++ internal/database/models.go
@@ -1,12 +1,12 @@
 package database
 
 type User struct {
-	ID         string `bson:"_id,omitempty"`
-	Username   string `bson:"username"`
-	Name       string `bson:"name"`
-	Banned     bool   `bson:"banned"`
-	RollNo     string `bson:"rollno"`
-	Department string `bson:"department"`
-	KratosID   string `bson:"kid"`
-	EmailID    string `bson:"email"`
+	ID         string `bson:"_id,omitempty" json:"id"`
+	Username   string `bson:"username" json:"username"`
+	Name       string `bson:"name" json:"name"`
+	Banned     bool   `bson:"banned" json:"banned"`
+	RollNo     string `bson:"rollno" json:"rollno"`
+	Department string `bson:"department" json:"department"`
+	KratosID   string `bson:"kid" json:"kid"`
+	EmailID    string `bson:"email" json:"email"`
 }
